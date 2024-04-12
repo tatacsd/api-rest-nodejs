f4b791a6a9ea51a572405cca9fb038a11742276e
Thays Casado <thayscasado@gmail.com>
2024-04-12 00:12:38 -0700

    - add get transactions route
    - add get all transactions

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -22,4 +22,26 @@ export async function TransactionsRoutes(app: FastifyInstance) {
         return reply.status(201).send();
       });
 
+      app.get("/", async (request, reply) => {
+        const transactions = await knex("transactions").select("*");
+
+        return reply.status(200).send({ transactions });
+      });
+
+      app.get("/:id", async (request, reply) => {
+        const getTransactionSchema = z.object({
+            id: z.string().uuid(),
+        });
+
+        const { id } = getTransactionSchema.parse(request.params);
+
+        const transaction = await knex("transactions").select("*").where({ id }).first();
+
+        if (!transaction) {
+            return reply.status(404).send();
+        }
+
+        return reply.status(200).send({ transaction });
+      });
+
 }
