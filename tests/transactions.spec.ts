52997bec78c694a8574258dab9e597cb5dbd331e
Thays Casado <thayscasado@gmail.com>
2024-04-13 15:38:19 -0700

    use describe to categorize the tests and using it instead of test
    add route validation for post and get creation;
    - good practice note: if a test need another test it should be a single test.
    - use describe to categorize the tests and using it instead of test

--- tests/transactions.spec.ts
+++ tests/transactions.spec.ts
@@ -1,22 +1,53 @@
-import { test, beforeAll, afterAll } from "vitest";
+import { it, beforeAll, afterAll, describe, expect} from "vitest";
 import request from "supertest";
 import { app } from "../src/app";
 
-beforeAll(async () => {
-  await app.ready();
-});
+describe("Transactions routes", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
 
-afterAll(async () => {
-  app.close();
-});
+  afterAll(async () => {
+    app.close();
+  });
 
-test("The user should create a transaction", async () => {
-  await request(app.server) // app.server is the http server instance
-    .post("/transactions") 
-    .send({ 
+  it("should be able to create a new transaction", async () => {
+    await request(app.server) // app.server is the http server instance
+      .post("/transactions")
+      .send({
+        title: "Salary",
+        amount: 1000,
+        type: "deposit",
+      })
+      .expect(201); // 201 Created
+  });
+
+  it("should be able to list the transactions", async () => {
+    const createTransactionResponse = await request(app.server)
+    .post("/transactions")
+    .send({
       title: "Salary",
       amount: 1000,
       type: "deposit",
-    })
-    .expect(201); // 201 Created
+    });
+
+    // get the cookies
+    const cookies = createTransactionResponse.header["set-cookie"];
+
+    const listTransactionsResponse = await request(app.server)
+      .get("/transactions")
+      .set("Cookie", cookies)
+      .expect(200);
+
+
+      // Validate the response body content
+      expect(listTransactionsResponse.body.transactions).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            title: "Salary",
+            amount: 1000,
+          })
+        ])
+      );
+  });
 });
